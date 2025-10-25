88741af5df8b79de50b3a34e607099f4d26ad1ad
dmusau23 <danmusau09@gmail.com>
2025-10-24 21:11:22 -0400

    Added some conflicts

--- 09-name.sh
+++ 09-name.sh
@@ -5,10 +5,11 @@
 servers="ggc.edu facebook.com cnn.com x.com france24.fr"
 
 for server in $servers; do
-    ping -c3 $server
-    echo "----------------------------"
+    traceroute $server
+    echo "========================-"
 done
 
 # exercise: Change the list of servers and also the 
 # operation applied to them. For instance, use ping, 
 # traceroute, or nslookup with other options.
+#completed the exercise
